edc23f547bb91de3e128d396bb5ad307c0bccd98
Charles Carriere <charles@revelry.co>
2018-02-03 19:12:09 -0600

    saving progress

--- discuss/web/controllers/topic_controller.ex
+++ discuss/web/controllers/topic_controller.ex
@@ -1,9 +1,15 @@
 defmodule Discuss.TopicController do
   use Discuss.Web, :controller
 
-  def new() do
+  alias Discuss.Topic
 
+  def new(conn, params) do  #new function needs to have a new template new.html.eex
+    changeset = Topic.changeset(%Topic{}, %{}) #we are passing these as struct and params; created changeset with Topic module and Topic struct
 
+    render conn, "new.html", changeset: changeset #b/c we are in TopicController it gets this from Topic Folder
   end
 
 end
+
+#passes in changeset or other custom data into new.html.eex
+# by defaults, templates always pass in conn as conn
