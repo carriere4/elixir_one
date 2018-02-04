14aa1ff02b022295ff680f9f4052cfb618313e77
Charles Carriere <charles@revelry.co>
2018-02-04 14:01:24 -0600

    stuck now so saving work

--- discuss/web/controllers/topic_controller.ex
+++ discuss/web/controllers/topic_controller.ex
@@ -3,10 +3,25 @@ defmodule Discuss.TopicController do
 
   alias Discuss.Topic
 
-  def new(conn, params) do  #new function needs to have a new template new.html.eex
+  def index(conn, _params) do
+    topics = Repo.all(Topic)
+    render conn, "index.html"
+  end
+
+  def new(conn, _params) do  #new function needs to have a new template new.html.eex
     changeset = Topic.changeset(%Topic{}, %{}) #we are passing these as struct and params; created changeset with Topic module and Topic struct
 
     render conn, "new.html", changeset: changeset #b/c we are in TopicController it gets this from Topic Folder
+  end
+
+  def create(conn, %{"topic" => topic}) do
+    changeset = Topic.changeset(%Topic{}, topic)
+
+    case Repo.insert(changeset) do
+      {:ok, post} -> IO.inspect(post)
+      {:error, changeset} ->
+        render conn, "new.html", changeset: changeset
+    end
   end
 
 end
